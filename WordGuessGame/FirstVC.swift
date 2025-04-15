7f5eb24c4eaa3c681d6ce68c0801b37dd6dbb949
Özcan <ozcanozdemir001@gmail.com>
2025-04-15 16:20:33 +0300

    First page buttons are added and bounce feature as well.

--- WordGuessGame/FirstVC.swift
+++ WordGuessGame/FirstVC.swift
@@ -11,28 +11,87 @@ class FirstVC: UIViewController {
     
     var backgroundImage = UIImageView()
     var wordImage = UIImageView()
+    let buttonStart = UIButton()
+    let buttonHighScore = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(ciColor: CIColor(red: 0.5, green: 1, blue: 0.8, alpha: 0.5))
         setUpUserInterfaces()
     }
     
     private func setUpUserInterfaces(){
         
-        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapGestureMethod))
-        view.addGestureRecognizer(tapGR)
+//        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapGestureMethod))
+//        view.addGestureRecognizer(tapGR)
+        let screenWidth = view.frame.width
         
-        backgroundImage.frame = view.bounds
-        backgroundImage.image = UIImage(named: "bb")
+        backgroundImage.frame = CGRect(x: 30, y: 80, width: screenWidth - 60, height: screenWidth - 60 )
+        backgroundImage.layer.cornerRadius = (screenWidth - 60)/2
+        backgroundImage.layer.borderWidth = 3
+        backgroundImage.layer.borderColor = UIColor.white.cgColor
+        backgroundImage.clipsToBounds = true
+        backgroundImage.image = UIImage(named: "gg")
         view.addSubview(backgroundImage)
+        
+        buttonStart.setTitleColor(.white, for: UIControl.State.normal)
+        buttonStart.frame = CGRect(x: 110, y: 700, width: screenWidth - 220, height: 50)
+        buttonStart.layer.cornerRadius = 5
+        buttonStart.layer.borderWidth = 0
+        buttonStart.setTitle("Start", for: .normal)
+        buttonStart.titleLabel?.font = UIFont.boldSystemFont(ofSize: 27)
+        buttonStart.backgroundColor = UIColor(ciColor: CIColor(red: 0.9, green: 0.5, blue: 0.1, alpha: 0.5))
+        buttonStart.addTarget(self, action: #selector(startButtonMethod), for: .touchUpInside)
+        
+        // Button yan gölgeleme ekleme kod bloğu
+        buttonStart.layer.shadowColor = UIColor.black.cgColor
+        buttonStart.layer.shadowOffset = CGSize(width: 4, height: 8)
+        buttonStart.layer.shadowOpacity = 0.9
+        buttonStart.layer.shadowRadius = 4
+        view.addSubview(buttonStart)
+        
+        
+        buttonHighScore.setTitleColor(.white, for: UIControl.State.normal)
+        buttonHighScore.frame = CGRect(x: 110, y: 630, width: screenWidth - 220, height: 50)
+        buttonHighScore.layer.cornerRadius = 5
+        buttonHighScore.layer.borderWidth = 0
+        buttonHighScore.setTitle("High Scores", for: .normal)
+        buttonHighScore.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        buttonHighScore.backgroundColor = UIColor(ciColor: CIColor(red: 0.9, green: 0.5, blue: 0.1, alpha: 0.5))
+        buttonHighScore.addTarget(self, action: #selector(highScoreButtonMethod), for: .touchUpInside)
+        
+        // Button yan gölgeleme ekleme kod bloğu
+        buttonHighScore.layer.shadowColor = UIColor.black.cgColor
+        buttonHighScore.layer.shadowOffset = CGSize(width: 4, height: 8)
+        buttonHighScore.layer.shadowOpacity = 0.9
+        buttonHighScore.layer.shadowRadius = 4
+        view.addSubview(buttonHighScore)
+        
+        
         
     }
     
-    @objc func tapGestureMethod() {
-        self.navigationController?.setViewControllers([SecondVC()], animated: true)
-        print("clicked")
+    @objc func startButtonMethod() {
+        self.animateButtonBounce(buttonStart)
+        self.navigationController?.pushViewController(SecondVC(), animated: true)
+//        self.navigationController?.setViewControllers([SecondVC()], animated: true)
     }
-
-
+    
+    @objc func highScoreButtonMethod() {
+        self.animateButtonBounce(buttonHighScore)
+        print("These are high Scores")
+    }
+    
+    func animateButtonBounce(_ button: UIButton) {
+        UIView.animate(withDuration: 0.1,
+                       animations: {
+                           button.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+                       },
+                       completion: { _ in
+                           UIView.animate(withDuration: 0.1) {
+                               button.transform = CGAffineTransform.identity
+                           }
+                       })
+    }
 }
 
--- WordGuessGame/SecondVC.swift
+++ WordGuessGame/SecondVC.swift
@@ -8,15 +8,47 @@
 import UIKit
 
 class SecondVC: UIViewController {
+    
+    let wordGuessTextField = UITextField()
+    let submitButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
+        setUpUIs()
+        
+        
     }
     
-
+    private func setUpUIs() {
+        
+        createTextField(placeHolder: "Enter words", top: 120, bottom: 710, leading: 15, trailing: 15)
+        
+        
+        
+    }
+    
+    func createTextField(placeHolder: String,top: Int, bottom: Int,leading :Int, trailing:Int ) {
+        let textField = UITextField()
+        textField.placeholder = placeHolder
+        textField.layer.cornerRadius = 6
+        textField.layer.borderWidth = 1
+        textField.textAlignment = .left
+        textField.backgroundColor = .white
+        view.addSubview(textField)
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: view.topAnchor, constant: CGFloat(top)),
+            textField.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: CGFloat(-bottom)),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: CGFloat(leading)),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: CGFloat(-trailing))
+        
+        ])
+        
+        
+    }
 
 }
