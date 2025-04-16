990dd5dbb0e947dde903b424c91a3fbbb0682111
Özcan <ozcanozdemir001@gmail.com>
2025-04-16 16:46:03 +0300

    second page some changes done

--- WordGuessGame/SecondVC.swift
+++ WordGuessGame/SecondVC.swift
@@ -9,27 +9,48 @@ import UIKit
 
 class SecondVC: UIViewController {
     
-    let wordGuessTextField = UITextField()
+    var wordGuessTextField = UITextField()
+    var userGuessTF = UITextField()
     let submitButton = UIButton()
+    let fourLetterWordArray: [String] = [
+        "blue", // adjective (mavi)
+        "kind", // adjective (nazik)
+        "frog", // noun (kurbağa)
+        "cool", // adjective (havalı)
+        "milk", // noun (süt)
+        "calm", // adjective (sakin)
+        "book", // noun (kitap)
+        "bold", // adjective (cesur)
+        "star", // noun (yıldız)
+        "cold", // adjective (soğuk)
+        "fire", // noun (ateş)
+        "dark", // adjective (karanlık)
+        "luck", // noun (şans)
+        "soft", // adjective (yumuşak)
+        "wind"  // noun (rüzgâr)
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = UIColor(ciColor: CIColor(red: 0.5, green: 1, blue: 0.8, alpha: 0.5))
         setUpUIs()
+        pickRandomWordFunc()
+        let screenWidth = view.frame.width
+        print(screenWidth)
     }
     
     private func setUpUIs() {
-        createTextField(placeHolder: "Enter words", top: 120, bottom: 710, leading: 15, trailing: 15)
-        createTextField(placeHolder: "Some words", top: 190, bottom: 640, leading: 15, trailing: 15)
+        wordGuessTextField = createTextField(placeHolder: "Enter words", top: 120, bottom: 710, leading: 15, trailing: 15)
+        userGuessTF = createTextField(placeHolder: "Some words", top: 190, bottom: 630, leading: 11, trailing: 331)
+        
     }
     
-    func createTextField(placeHolder: String,top: Int, bottom: Int,leading :Int, trailing:Int ) {
+    func createTextField(placeHolder: String,top: Int, bottom: Int,leading :Int, trailing:Int ) -> UITextField {
         let textField = UITextField()
         textField.placeholder = placeHolder
         textField.layer.cornerRadius = 6
 //        textField.layer.borderWidth = 1
-        textField.textAlignment = .left
+        textField.textAlignment = .center
         textField.backgroundColor = .white
         view.addSubview(textField)
         
@@ -43,7 +64,15 @@ class SecondVC: UIViewController {
         
         ])
         
-        
+        return textField
+    }
+    
+    func pickRandomWordFunc() {
+        if let randomName = fourLetterWordArray.randomElement() {
+            print("\(randomName)")
+            self.wordGuessTextField.text = randomName
+
+        }
     }
 
 }
