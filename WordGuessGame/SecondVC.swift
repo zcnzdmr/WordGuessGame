cba38376264c1ba368a1dd94e6a4dfc1cc4444e3
Özcan <ozcanozdemir001@gmail.com>
2025-04-18 11:51:33 +0300

    mmm

--- WordGuessGame/SecondVC.swift
+++ WordGuessGame/SecondVC.swift
@@ -17,6 +17,7 @@ class SecondVC: UIViewController {
     var userGuessTF5 = UITextField()
     var userGuessTF6 = UITextField()
     let submitButton = UIButton()
+    var randomName = ""
     let fourLetterWordArray: [String] = [
         "blue", // adjective (mavi)
         "kind", // adjective (nazik)
@@ -32,13 +33,35 @@ class SecondVC: UIViewController {
         "dark", // adjective (karanlık)
         "luck", // noun (şans)
         "soft", // adjective (yumuşak)
-        "wind"  // noun (rüzgâr)
+        "wind" , // noun (rüzgâr),
+        "apple", // noun
+        "brave", // adjective
+        "crane", // noun/verb
+        "daisy", // noun
+        "eagle", // noun
+        "flame", // noun
+        "grape", // noun
+        "happy", // adjective
+        "jolly", // adjective
+        "knife",
+        "animal", // noun
+        "battle", // noun
+        "castle", // noun
+        "danger", // noun
+        "effort", // noun
+        "forest", // noun
+        "galaxy", // noun
+        "hunter", // noun
+        "island", // noun
+        "jungle"  // noun
     ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(ciColor: CIColor(red: 0.5, green: 1, blue: 0.8, alpha: 0.5))
         setUpUIs()
+        randomName = fourLetterWordArray.randomElement() ?? ""
+        self.wordGuessTextField.text = randomName
         pickRandomWordFunc()
     }
     
@@ -46,72 +69,48 @@ class SecondVC: UIViewController {
         
         // O kadar modüler olmayan bir kod bloğu ki ben bile utandım yazarken
         wordGuessTextField = createTextField(placeHolder: "Enter words", top: 120, width: 380, leading: 11, height: 50)
-        userGuessTF1 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 11, height: 45)
-        userGuessTF2 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 77, height: 45)
-        userGuessTF3 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 142, height: 45)
-        userGuessTF4 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 209, height: 45)
-        userGuessTF5 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 275, height: 45)
-        userGuessTF6 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 341, height: 45)
+        userGuessTF1 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 11, height: 45,  tag: 0)
+        userGuessTF2 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 77, height: 45,  tag: 1)
+        userGuessTF3 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 142, height: 45, tag: 2)
+        userGuessTF4 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 209, height: 45, tag: 3)
+        userGuessTF5 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 275, height: 45, tag: 4)
+        userGuessTF6 = createTextField(placeHolder: "_", top: 190, width: 50, leading: 341, height: 45, tag: 5)
     }
     
     func createTextField(placeHolder: String,top: Int, width: Int,leading :Int, height:Int, tag : Int = 0 ) -> UITextField {
         let textField = UITextField()
         textField.placeholder = placeHolder
         textField.layer.cornerRadius = 6
-//        textField.layer.borderWidth = 1
         textField.textAlignment = .center
         textField.backgroundColor = .white
         textField.layer.borderWidth = 0.5
         textField.autocorrectionType = .no
         textField.tag = tag
+        textField.addTarget(self, action: #selector(guessWordFuncTF), for: .editingChanged)
         textField.frame = CGRect(x: leading, y: top, width: width, height: height)
         view.addSubview(textField)
-
         
-//        textField.translatesAutoresizingMaskIntoConstraints = false
-        
-//        NSLayoutConstraint.activate([
-//            textField.topAnchor.constraint(equalTo: view.topAnchor, constant: CGFloat(top)),
-//            textField.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: CGFloat(-bottom)),
-//            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: CGFloat(leading)),
-//            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: CGFloat(-trailing))
-//        ])
-        
+        textField.delegate = self
         return textField
     }
     
     func pickRandomWordFunc() {
-        if let randomName = fourLetterWordArray.randomElement() {
-            print("\(randomName)")
-            self.wordGuessTextField.text = randomName
-            if randomName.count > 3 {
+        if randomName.count > 3 && randomName.count < 5 {
                 userGuessTF5.backgroundColor = UIColor.systemYellow
                 userGuessTF2.backgroundColor = UIColor.systemYellow
                 userGuessTF3.backgroundColor = UIColor.systemYellow
                 userGuessTF4.backgroundColor = UIColor.systemYellow
                 userGuessTF1.isHidden = true
                 userGuessTF6.isHidden = true
-                
-                if let one = userGuessTF1.text {
-                    if one.isEmpty && one.count > 1 {
-                        print("uyarı1")
-                    }else {
-                            if randomName.first == Character(one) {
-                                userGuessTF1.backgroundColor = .green
-                            }else{
-                            }
-                    }
-                    
-                }
 
-            }else if randomName.count > 4 {
+            }else if randomName.count > 4 && randomName.count < 6 {
                 userGuessTF1.backgroundColor = UIColor.systemYellow
                 userGuessTF2.backgroundColor = UIColor.systemYellow
                 userGuessTF3.backgroundColor = UIColor.systemYellow
                 userGuessTF4.backgroundColor = UIColor.systemYellow
                 userGuessTF5.backgroundColor = UIColor.systemYellow
                 userGuessTF6.isHidden = true
-            }else {
+            }else if randomName.count >= 6 {
                 userGuessTF1.backgroundColor = UIColor.systemYellow
                 userGuessTF2.backgroundColor = UIColor.systemYellow
                 userGuessTF3.backgroundColor = UIColor.systemYellow
@@ -120,8 +119,30 @@ class SecondVC: UIViewController {
                 userGuessTF6.backgroundColor = UIColor.systemYellow
             }
         }
-    }
     
+    
+    @objc func guessWordFuncTF() {
+        
+        guard var one = userGuessTF1.text else {
+            print("hata1")
+            return
+        }
+        one = one.lowercased()
+        print(one)
+ 
+            if randomName.first == Character(one) {
+                userGuessTF1.backgroundColor = UIColor.systemGreen
+            
+        }
+    
+    }
+}
+
+
+extension SecondVC: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return (string.count <= 1)
+    }
 }
 
 
